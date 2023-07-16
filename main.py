f6aac0c15957e399b1d1385b1c25e54ac3d584b2
Park Jinhwan <106535998+pjh09050@users.noreply.github.com>
2023-07-17 00:36:41 +0900

    update

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ warnings.filterwarnings('ignore')
 objective_list = ['total_flowtime', 'makespan', 'tardy_job', 'total_tardiness', 'total_weighted_tardiness']
 learning_rate = 0.0005
 gamma = 1
-buffer_limit = 10000
+buffer_limit = 50000
 batch_size = 32
-num_episodes = 21
+num_episodes = 1000
 
 class ReplayBuffer():
     def __init__(self):
@@ -22,11 +22,10 @@ class ReplayBuffer():
 
     def put(self, transition):
         self.buffer.append(transition)
-        #print(transition)
 
     def sample(self, n):
         mini_batch = random.sample(self.buffer, n)
-        s_lst, a_lst, r_lst, s_prime_lst, done_mask_lst = [], [], [], [], [] 
+        s_lst, a_lst, r_lst, s_prime_lst, done_mask_lst, result_lst = [], [], [], [], [], []
         for transition in mini_batch:
             s, a, r, s_prime, done_mask = transition # done_mask : 종료 상태의 밸류를 마스킹해주기 위해 만든 변수
             s_lst.append(s) # 스칼라 값이 아닌 벡터 값이 나올 수 있기 때문
@@ -46,48 +45,58 @@ class Qnet(nn.Module):
         self.fc1 = nn.Linear(100, 128)
         self.fc2 = nn.Linear(128, 128)
         self.fc3 = nn.Linear(128, 100)
-        self.selected_jobs = set()
+        self.remaining_jobs = list(range(1, 101))
+        self.pre_jobs = []
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+    
+    def reset(self):
+        self.remaining_jobs = list(range(1, 101))
+        self.pre_jobs = []
 
     def sample_action(self, obs, epsilon):
         out = self.forward(obs)
         coin = random.random()
         if coin < epsilon:
-            return random.randint(1, 100)
+            action = random.choice(self.remaining_jobs)
+            self.remaining_jobs.remove(action)
+            self.pre_jobs.append(action)
+            return action
         else:
-            max_q = float('-inf')
-            max_action = None
-            for action, q_value in enumerate(out, 1):
-                if action in self.selected_jobs:
-                    continue  # 이전에 선택한 작업은 건너뜀
-                if q_value > max_q:
-                    max_q = q_value
-                    max_action = action
-            if max_action is None:
-                return random.randint(1, 100)
-            else:
-                self.selected_jobs.add(max_action)  # 선택한 작업을 기록
-                return max_action
+            q_values = out.detach().numpy()
+            for action_index in self.pre_jobs:
+                q_values[action_index-1] = float('-inf')
+            max_action = np.argmax(q_values)
+            max_action += 1
+            self.remaining_jobs.remove(max_action)
+            self.pre_jobs.append(max_action)
+            return max_action
+    
+    def select_action(self, obs):
+        out = self.forward(obs)
+        q_values = out.detach().numpy()
+        for action_index in self.pre_jobs:
+            q_values[action_index-1] = float('-inf')
+        max_action = np.argmax(q_values)
+        max_action += 1
+        self.remaining_jobs.remove(max_action)
+        self.pre_jobs.append(max_action)
+        return max_action            
 
 class Single_machine():
     def __init__(self, df):
         self.x = [0 for i in range(100)]
         self.stop = 0
         self.df = df
-        self.prev_action = None
 
     def step(self, a):
-        if self.prev_action is not None:
-            while a == self.prev_action:
-                a = random.randint(1, 100)
         self.x[self.stop] = a
         self.stop += 1
-        reward = 100 - self.get_fitness(self.x)
+        reward = - self.get_fitness(self.x)
         done = self.is_done()
         self.prev_action = a
         return self.x, reward, done
@@ -100,9 +109,8 @@ class Single_machine():
 
     def reset(self):
         self.stop = 0
-        self.job_sequence = [0 for i in range(100)]
-        self.prev_action = None
-        return self.job_sequence
+        self.x= [0 for _ in range(100)]
+        return self.x
     
     def get_fitness(self,sequence):
         flowtime = 0
@@ -115,7 +123,6 @@ class Single_machine():
 
         for i in sequence:
             if i == 0:
-                total_tardiness += 1000000
                 break
             job = self.df['job' + str(i)]
             flowtime += job['소요시간']
@@ -131,18 +138,21 @@ class Single_machine():
 
             weighted_tardiness = job['성적 반영비율'] * tardiness
             total_weighted_tardiness += weighted_tardiness
-        ob_list = {'total_flowtime':total_flowtime, 'makespan':makespan, 'tardy_job':tardy_job,'total_tardiness':total_tardiness, 'total_weighted_tardiness':total_weighted_tardiness}
-
-        return ob_list[objective_list[3]]
+        return tardiness
 
 # 학습 함수        
 def train(q, q_target, memory, optimizer):
     for i in range(10): # 1번의 업데이트에 32개의 데이터가 사용됨, 한 에피소드가 끝날 때마다 버퍼에서 총 320개의 데이터를 뽑아서 사용함
         s, a, r, s_prime, done_mask = memory.sample(batch_size) # 리플레이 버퍼에서 미니 배치 추출
         q_out = q(s)
-        print(a)
-        q_a = q_out.gather(1, a) # 실제 선택된 액션의 q값을 의미 # a에 해당하는 q값 추출
-        max_q_prime = q_target(s_prime).max(1)[0].unsqueeze(1) # q_target 네트워크는 정답지를 계산할 떄 쓰이는 네트워크로 학습 대상이 아니다.
+        q_a = q_out.gather(1, a-1) # 실제 선택된 액션의 q값을 의미 # a에 해당하는 q값 추출
+        ad_q_target = q_target(s_prime)
+        for j in range(len(q_target(s_prime))):
+            ad_s = s[j].tolist()
+            ad_s = [int(x) for x in ad_s if int(x)!=0 ]
+            for action_index in ad_s:
+                ad_q_target[j][action_index-1] = float('-inf')
+        max_q_prime = ad_q_target.max(1)[0].unsqueeze(1) # q_target 네트워크는 정답지를 계산할 떄 쓰이는 네트워크로 학습 대상이 아니다.
         target = r + gamma * max_q_prime * done_mask
         loss = F.smooth_l1_loss(q_a, target) # 손실 함수 정의
         optimizer.zero_grad()
@@ -157,13 +167,14 @@ def main():
     q_target.load_state_dict(q.state_dict())
     memory = ReplayBuffer()
 
-    print_interval = 20
+    print_interval = 1
     score = 0.0
     optimizer = optim.Adam(q.parameters(), lr=learning_rate)
 
     for n_epi in range(num_episodes):
-        epsilon = max(0.01, 0.08-0.01*(n_epi/200))
+        epsilon = max(0.01, 0.08-0.03*(n_epi/200))
         s = env.reset()
+        q.reset()
         s = np.array(s)
         done = False
 
@@ -185,7 +196,23 @@ def main():
         if n_epi%print_interval==0 and n_epi!=0:
             q_target.load_state_dict(q.state_dict()) # q네트워크의 파라미터를 q_target네트워크로 복사
             print("n_episode : {}, score : {:.1f}, n_buffer : {}, eps : {:.1f}%".format(n_epi, score/print_interval, memory.size(), epsilon*100))
-            score = 0.0
+        score = 0.0
+
+    s = env.reset()
+    q.reset()
+    s = np.array(s)
+    done = False
+    while not done:
+        a = q.select_action(torch.from_numpy(s).float())
+        s_prime, r, done = env.step(a)
+        s = np.array(s)
+        s_prime = np.array(s_prime)
+        done_mask = 0.0 if done else 1.0
+        memory.put((s, a, r, s_prime, done_mask))
+        s = s_prime
+        score += r
+        if done:
+            break
 
 if __name__ == '__main__':
     main()    
