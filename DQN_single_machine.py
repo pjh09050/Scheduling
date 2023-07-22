09aa1e24a6deb1cff959b8dcb70d2bdfd4f85992
Park Jinhwan <106535998+pjh09050@users.noreply.github.com>
2023-07-23 00:36:20 +0900

    update

--- DQN_single_machine.py
+++ DQN_single_machine.py
@@ -8,7 +8,6 @@ import numpy as np
 import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
-from Single_machine import Single_machine
 
 learning_rate = 0.0005
 gamma = 1
@@ -25,7 +24,7 @@ class ReplayBuffer():
 
     def sample(self, n):
         mini_batch = random.sample(self.buffer, n)
-        s_lst, a_lst, r_lst, s_prime_lst, done_mask_lst, result_lst = [], [], [], [], [], []
+        s_lst, a_lst, r_lst, s_prime_lst, done_mask_lst = [], [], [], [], []
         for transition in mini_batch:
             s, a, r, s_prime, done_mask = transition # done_mask : 종료 상태의 밸류를 마스킹해주기 위해 만든 변수
             s_lst.append(s) # 스칼라 값이 아닌 벡터 값이 나올 수 있기 때문
@@ -50,23 +49,97 @@ class Qnet(nn.Module):
         x = torch.relu(self.fc2(x))
         x = self.fc3(x)
         return x
-    
-    def reset(self):
-        self.remaining_jobs = list(range(1, 101))
-        self.pre_jobs = []
 
     def sample_action(self, obs, epsilon):
         out = self.forward(obs)
         coin = random.random()
         if coin < epsilon:
-            return random.randint(0, 1)
+            return random.randint(0, 2)
         else:
             return out.argmax().item()
     
     def select_action(self, obs):
         out = self.forward(obs)
         return out.argmax().item()
+    
+class Single_machine():
+    def __init__(self, df):
+        self.x = [0, 0, 0]
+        self.stop = 0
+        self.df = df
+        self.jobs_info = {i: self.df['job' + str(i)] for i in range(1, 101)}
+        self.act = []
+        self.seq = [i for i in range(1, 101)]
+        self.memo = {}
 
+    def step(self, a):
+        select_job = self.dispatching_act(a)
+        self.act.append(select_job)
+        self.stop += 1
+        reward = - self.get_fitness(self.act)[0]
+        total_flowtime = self.get_fitness(self.act)[1]
+        done = self.is_done()
+        self.x = [total_flowtime, self.stop, 100-self.stop]
+        return self.x, reward, done
+
+    def dispatching_act(self, a):
+        if a == 0:  # SPT
+            self.seq.sort(key=lambda x: self.jobs_info[x]['소요시간'])
+        elif a == 1:  # EDD
+            self.seq.sort(key=lambda x: self.jobs_info[x]['제출기한'])
+        else:  # slack time
+            self.seq.sort(key=lambda x: self.jobs_info[x]['제출기한'] - self.jobs_info[x]['소요시간'])
+        chosen_job = self.seq.pop(0)
+        return chosen_job
+    
+    # def get_fitness(self,sequence):
+    #     flowtime = 0
+    #     total_flowtime = 0
+    #     tardiness = 0
+    #     total_tardiness = 0
+    #     tardy_job = 0
+    #     for i in sequence:
+    #         if i == 0:
+    #             break
+    #         job = self.df['job' + str(i)]
+    #         flowtime += job['소요시간']
+    #         total_flowtime += flowtime
+    #         tardiness = max(flowtime - job['제출기한'], 0)
+    #         total_tardiness += tardiness
+    #         tardy_job += 1 if tardiness > 0 else 0
+    #     return tardiness, total_flowtime
+
+    def get_fitness(self, sequence):
+        key = tuple(sequence)  # sequence 리스트를 튜플로 변환하여 키로 활용
+        if key in self.memo:
+            return self.memo[key]
+        flowtime = 0
+        total_flowtime = 0
+        tardiness = 0
+        for i in sequence:
+            if i == 0:
+                break
+            job = self.df['job' + str(i)]
+            flowtime += job['소요시간']
+            total_flowtime += flowtime
+            tardiness = max(flowtime - job['제출기한'], 0)
+        result = (tardiness, total_flowtime)
+        self.memo[key] = result
+        return result
+    
+    def is_done(self):
+        if self.stop == 100:
+            return True
+        else: 
+            return False
+
+    def reset(self):
+        self.stop = 0
+        self.x= [0, 0, 0] # [ 그때 state의 total_flowtime, 인덱스 위치, 남은 job 수 ]
+        self.act = []
+        self.seq = [i for i in range(1, 101)]
+        return self.x
+    
 def train(q, q_target, memory, optimizer):
     for i in range(10): # 1번의 업데이트에 32개의 데이터가 사용됨, 한 에피소드가 끝날 때마다 버퍼에서 총 320개의 데이터를 뽑아서 사용함
         s, a, r, s_prime, done_mask = memory.sample(batch_size) # 리플레이 버퍼에서 미니 배치 추출
@@ -78,7 +151,6 @@ def train(q, q_target, memory, optimizer):
         optimizer.zero_grad() # 현재 배치에 대한 그라디언트를 계산하기 전에 모델의 매개변수 그라디언트를 초기화하는 역할
         loss.backward() # loss에 대한 그라디언트 계산이 일어남
         optimizer.step() # Qnet의 파라미터의 업데이트가 일어남
-
 
 def main():
     df = pd.read_csv('100_job_uniform data.csv', index_col=0)
@@ -95,9 +167,9 @@ def main():
     for n_epi in range(num_episodes):
         epsilon = max(0.01, 0.08-0.03*(n_epi/200))
         s = env.reset()
-        q.reset()
         s = np.array(s)
         done = False
+        score = 0.0
 
         while not done:
             a = q.sample_action(torch.from_numpy(s).float(), epsilon)
@@ -117,25 +189,24 @@ def main():
         if n_epi%print_interval==0 and n_epi!=0:
             q_target.load_state_dict(q.state_dict())
             print("n_episode : {}, score : {:.1f}, n_buffer : {}, eps : {:.1f}%".format(n_epi, score, memory.size(), epsilon*100))
-        score = 0.0
-
+        
     s = env.reset()
-    q.reset()
     s = np.array(s)
     done = False
+    score = 0.0
     while not done:
         a = q.select_action(torch.from_numpy(s).float())
         s_prime, r, done = env.step(a)
         s = np.array(s)
         s_prime = np.array(s_prime)
-        done_mask = 0.0 if done else 1.0
-        memory.put((s, a, r, s_prime, done_mask))
         s = s_prime
         score += r
         if done:
             break
-        return score
+    return score
 
 if __name__ == '__main__':
     score = main()    
     print('Score', score)
+
+    
